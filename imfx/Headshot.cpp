ab4f1b4c80e9064739b8bdfc7975dc3ab2455ad6
DK22Pac <gdamdk@gmail.com>
2017-05-01 03:37:21 +0300

    Headshot code fixes

--- imfx/Headshot.cpp
+++ imfx/Headshot.cpp
@@ -3,8 +3,6 @@
 
 void Headshot::DoHeadshot(CPed *ped, int bodyPartToRemove) {
     if (bodyPartToRemove == 2 && ped->m_apBones[bodyPartToRemove]->m_pIFrame) {
-        ped->m_nPedFlags.bRemoveHead = true;
-        ped->m_nBodypartToRemove = bodyPartToRemove;
         RwV3d position;
         ped->GetBonePosition(position, 0, true);
         FxSystem_c *particle = g_fxMan.CreateFxSystem("headshot", &position, 0, 0);
